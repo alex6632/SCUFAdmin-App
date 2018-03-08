142e0374d1d48e76b4407445d7363d436f601b2a
Alexandre Simonin <a.simonin69@gmail.com>
2018-03-08 08:16:24 +0100

    INTE login page + page header

--- www/js/app.js
+++ www/js/app.js
@@ -10,6 +10,24 @@ var me = {
       var current = $(this).attr('data-routing');
       $('.routing#' + current).addClass('show');
     });
+  },
+  login: function() {
+    $('#jsShowConnectForm').on('click', function () {
+      $(this).parents('.login').addClass('step2');
+      $('.login__info-touch').fadeOut();
+      $('.login__form').delay(800).fadeIn();
+    });
+  },
+  input:function(element) {
+    var myElement = $('#' + element);
+    myElement.keyup(function() {
+      var n = myElement.val();
+      if (n != "") {
+        myElement.prev().addClass('move');
+      } else {
+        myElement.prev().removeClass('move');
+      }
+    });
   }
 };
 
@@ -17,3 +35,7 @@ me.routing('planning');
 me.routing('validation');
 me.routing('actions');
 me.routing('profile');
+
+me.login();
+me.input('login-email');
+me.input('login-pwd');
