855687199a94eb30e4fe0c0468f49f4dd55392d8
Alexandre Simonin <a.simonin69@gmail.com>
2018-03-13 08:03:54 +0100

    INTE action page + WIP validation page

--- www/js/app.js
+++ www/js/app.js
@@ -11,6 +11,33 @@ var me = {
       $('.routing#' + current).addClass('show');
     });
   },
+  routingLevel2: function (element) {
+    $('.' + element).on('click', function () {
+      var content = $(this).text();
+      $(this).parents('.level1').addClass('swipe');
+      $('#' + element).addClass('show');
+      $(this).parents('.routing').find('.jsGoLevel1').addClass('show');
+      $(this).parents('.routing').find('.jsGoSearch').addClass('hide');
+      $(this).parents('.routing').find('.header__lvl2').text(content).addClass('show');
+    });
+    $('.jsGoLevel1').on('click', function () {
+      $(this).removeClass('show');
+      $(this).prev().removeClass('hide');
+      $(this).parents('.routing').find('.level1').removeClass('swipe');
+      $(this).parents('.routing').find('.level2').removeClass('show');
+      $(this).parents('.routing').find('.header__lvl2').text('').removeClass('show');
+    });
+  },
+  fadeInPage: function (element) {
+    $('.' + element).on('click', function () {
+      var elementToShow = $('#' + element);
+      if(elementToShow.css('display') == 'none') {
+        elementToShow.fadeIn();
+      } else {
+        elementToShow.fadeOut();
+      }
+    })
+  },
   login: function() {
     $('#jsShowConnectForm').on('click', function () {
       $(this).parents('.login').addClass('step2');
@@ -53,6 +80,7 @@ var me = {
     el.bind('touchend', function (e){
       var te = e.originalEvent.changedTouches[0].clientX;
       if(ts > te+5){
+        $('.notification__list__item').removeClass('swipe');
         $(this).addClass('swipe');
       }else if(ts < te-5){
         $(this).removeClass('swipe');
@@ -65,6 +93,12 @@ me.routing('planning');
 me.routing('validation');
 me.routing('actions');
 me.routing('profile');
+
+me.routingLevel2('rest');
+me.routingLevel2('leave');
+me.routingLevel2('hours');
+
+me.fadeInPage('jsNotifications');
 
 me.login();
 me.input('login-email');
